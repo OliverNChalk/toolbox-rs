0dbd936d40a2dbb2d506fdf05952da7fc9a0a641
OliverNChalk <11343499+OliverNChalk@users.noreply.github.com>
2025-05-27 19:19:43 -0500

    feat(fs): add `must_write` helper

--- src/fs.rs
+++ src/fs.rs
@@ -4,3 +4,8 @@ pub fn must_read(path: &Path) -> Vec<u8> {
     std::fs::read(path)
         .unwrap_or_else(|err| panic!("Failed to read provided file; err={err}; path={path:?}"))
 }
+
+pub fn must_write(path: &Path, data: &[u8]) {
+    std::fs::write(path, data)
+        .unwrap_or_else(|err| panic!("Failed to read provided file; err={err}; path={path:?}"));
+}
